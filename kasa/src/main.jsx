ffa5829d1267b39b21bdd3c8c6c0243eac2578aa
Jenni <jennifer.1707@hotmail.fr>
2024-03-12 16:57:48 +0100

    version 1.1

--- kasa/src/main.jsx
+++ kasa/src/main.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/index.css";
+
+const Home = () => (
+  <div>
+    <h2>Bienvenue sur la page d'accueil !</h2>
+    <p>C'est une application React avec React Router.</p>
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <h1>Mon Application React</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          {/* Ajoutez d'autres liens pour les pages supplémentaires */}
+          {/* <li>
+            <Link to="/autre-page">Autre Page</Link>
+          </li> */}
+        </ul>
+      </nav>
+      <hr />
+      <Route exact path="/" component={Home} />
+      {/* Ajoutez d'autres routes pour les pages supplémentaires */}
+      {/* <Route path="/autre-page" component={AutrePage} /> */}
+    </div>
+  </Router>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
