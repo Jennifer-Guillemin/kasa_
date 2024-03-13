ebb7e26e7480d2e869afb3b2cedd70c9e3c9fee0
Jenni <jennifer.1707@hotmail.fr>
2024-03-13 18:41:36 +0100

    version 1.3

--- kasa/src/components/Home.jsx
+++ kasa/src/components/Home.jsx
@@ -12,15 +12,16 @@ function Home() {
           <Link to="/">A Propos</Link>
         </nav>
       </banner>
-      <body>
-        <section className="image-section">
-          <img className="mer" src="./src/assets/IMG.png" alt="" />
-          <div>
-            <h1>Chez vous, partout et ailleurs</h1>
-          </div>
-        </section>
-        <section className="gallery"></section>
-      </body>
+
+      <section className="image-section">
+        <img className="mer" src="./src/assets/IMG.png" alt="" />
+        <div>
+          <h1>Chez vous, partout et ailleurs</h1>
+        </div>
+      </section>
+      <section className="gallery"></section>
+
+      <footer></footer>
     </div>
   );
 }
