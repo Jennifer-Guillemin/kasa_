3805194fe35713814b410b497b5577473a2fb0d3
Jenni <jennifer.1707@hotmail.fr>
2024-03-28 12:48:33 +0100

    version 2.3

--- kasa/src/components/Slideshow.jsx
+++ kasa/src/components/Slideshow.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Carousel = ({ images, alt }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
